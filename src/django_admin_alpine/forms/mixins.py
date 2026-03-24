8a3424d703bfc1fa8e36e05a0357bea652a533c7
Rodolfo Becerra <rodolvbg@gmail.com>
2026-03-24 14:24:18 +0100

    feat: update AdminAlpineMixin to use configurable JS path for Alpine.js

--- src/django_admin_alpine/forms/mixins.py
+++ src/django_admin_alpine/forms/mixins.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.forms import Media, Script
 
 
 class AdminAlpineMixin:
     @property
     def media(self):
+        alpine_js_path = getattr(
+            settings,
+            "DJANGO_ADMIN_ALPINE_JS_PATH",
+            "django_admin_alpine/js/alpine.js",
+        )
         return Media(
             js=(
                 Script("django_admin_alpine/js/admin.js", defer=True),
-                Script("django_admin_alpine/js/alpine.js", defer=True),
+                Script(alpine_js_path, defer=True),
             ),
         )
