670d08ce35cded7835241457b40843edde75520c
Rodolfo Becerra <rodolvbg@gmail.com>
2026-03-24 14:25:48 +0100

    feat: reintroduce AdminAlpineMixin and its media handling in mixins.py

---
+++ src/django_admin_alpine/__init__.py
@@ -0,0 +1,3 @@
+from django_admin_alpine.mixins import AdminAlpineMixin
+
+__all__ = ["AdminAlpineMixin"]
--- src/django_admin_alpine/forms/__init__.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class AdminAlpineFormMixin:
-    @property
-    def media(self):
-        return super().media + forms.Media(js=("django_admin_alpine/alpine.js",))
